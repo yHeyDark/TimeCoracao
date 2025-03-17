6970476696cd6f3ae268b7ae3d2353cf669d2a97
yheydark <122660784+yHeyDark@users.noreply.github.com>
2025-03-17 20:55:44 -0300

    GOlden State WArrios

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Gol");
+
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Den");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write(" Sta");
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("te");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write(" Wa");
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("rri");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("ors");
+
+
+Console.ResetColor();
+//Golden State Warriors azul e amarelo
+
